f7b6a4e8348d3f1a460cf0c10f0679a37cf02b1e
potaski <potaski@qq.com>
2018-12-13 17:14:16 +0800

    增加例子；删除文件写入部分冗余

--- file_protection/how_to.py
+++ file_protection/how_to.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
@@ -23,23 +23,55 @@ def example():
     key = RSA.generate(bits=4096)
     pri = key.exportKey(passphrase=passphrase, pkcs=8)
     pub = key.publickey().exportKey()
-    with open('id_rsa', 'wb') as f:
-        f.write(pri)
-    with open('id_rsa.pub', 'wb') as f:
-        f.write(pub)
     # encrypt file by pub
-    data = open('data_file', 'rb').read()
+    data = b'hello world!'
+    print('origin \n', data)
     pub_key = RSA.importKey(
-        externKey=open('id_rsa.pub').read(), passphrase=passphrase
+        externKey=pub.decode('utf-8'), passphrase=passphrase
     )
     encrypt_data = pub_key.encrypt(plaintext=data, K=Random.new())[0]
-    with open('data_file.encrypt', 'wb') as f:
-        f.write(encrypt_data)
     # decrypt file by pri
+    print('encrypt \n', encrypt_data)
     pri_key = RSA.importKey(
-        externKey=open('id_rsa').read(), passphrase=passphrase
+        externKey=pri.decode('utf-8'), passphrase=passphrase
     )
-    encrypt_data = open('data_file.encrypt', 'rb').read()
     data = pri_key.decrypt(ciphertext=encrypt_data)
-    with open('data_file.decrypt', 'wb') as f:
-        f.write(data)
+    print('decrypt \n', data)
+
+
+def example_2():
+    from Crypto.Cipher import PKCS1_v1_5
+    from Crypto.PublicKey import RSA
+    from Crypto.Hash import SHA
+    from Crypto import Random
+    data = b'hello world!\n'
+    print('origin \n', data)
+    with open('test/data', 'wb') as f: f.write(data)
+    h = SHA.new(data)
+    # generate random RSA key
+    passphrase = None
+    key = RSA.generate(bits=2048)
+    pri = key.exportKey()
+    with open('test/pri.key', 'wb') as f: f.write(pri)
+    pub = key.publickey().exportKey()
+    with open('test/pub.key', 'wb') as f: f.write(pub)
+    # encrypt file by pub
+    pub_key = RSA.importKey(pub.decode('utf-8'))
+    cipher = PKCS1_v1_5.new(pub_key)
+    ciphertext = cipher.encrypt(data + h.digest())
+    print('encrypt \n', ciphertext)
+    with open('test/data.1', 'wb') as f: f.write(ciphertext)
+    # decrypt
+    pri_key = RSA.importKey(pri.decode('utf-8'))
+    dsize = SHA.digest_size
+    sentinel = Random.new().read(15+dsize)
+    cipher = PKCS1_v1_5.new(pri_key)
+    data = cipher.decrypt(ciphertext, sentinel)
+    print('after decrypt \n', data)
+    digest = SHA.new(data[:-dsize]).digest()
+    print('method \n', data[:-dsize])
+    
+
+if __name__ == '__main__':
+    # example()
+    example_2()
